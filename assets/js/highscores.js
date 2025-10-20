e52901b0c618a909efa1d7a39d0ee3db5591306f
Alex-L5 <almenkiti@aol.com>
2025-10-20 10:16:56 +0100

    Add questions.json, update game.css, game.js, end.js and end.html, and change background colour in app.css

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,5 +1,5 @@
 const highScoresList = document.getElementById("highScoresList"); // get a reference to the highscores list
-const highScores = JSON.parse(localStorage.getItem("highScores")) || []; // get the list out of local storage, or empty array
+const highScores = JSON.parse(localStorage.getItem("highScores")) || []; // get the unordered list out of local storage, or empty array
 // console.log(highscores)                                 // check the command result is correct
 
 highScoresList.innerHTML = highScores               //add ordered list to each unordered list
