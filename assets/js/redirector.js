000c6cda52921c7fccd9f0b96243262ce4f51241
Alex-L5 <almenkiti@aol.com>
2025-11-01 08:26:02 +0000

    Update js file

--- assets/js/redirector.js
+++
@@ -1,9 +0,0 @@
-const VALID_PATHS = ["/", "/index.html", "/game.html","/highscores.html", "/end.html"];
-
-document.addEventListener("DOMContentLoaded", () => {
-  const currentPath = window.location.pathname;
-  const isKnown = VALID_PATHS.includes(currentPath);
-  if (!isKnown) {
-    window.location.replace("/index.html");
-  }
-});
